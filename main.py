a4804d1d258cde9b7713108a4305a964ac408cfd
John Bieren <jbieren64@gmail.com>
2025-04-13 18:50:29 -0700

    re-word usage log

--- main.py
+++ main.py
@@ -14,39 +14,39 @@ configure_logger()
 def remove_duplicates(directory):
     '''Remove duplicate .eml files from directory'''
     file_names = set()
-    files = duplicates = 0
+    emls_scanned = duplicates = 0
     directory_path = path.realpath(directory)
     for file_name in tqdm(listdir(directory_path)):
         if file_name[-4:] != ".eml":
             continue
-        files += 1
+        emls_scanned += 1
         potential_duplicate_part = file_name.split(".", maxsplit=1)[0]
         if potential_duplicate_part in file_names:
             remove(path.join(directory_path, file_name))
             duplicates += 1
         else:
             file_names.add(potential_duplicate_part)
-    return files, duplicates
+    return emls_scanned, duplicates
 
-def log_usage(start_time, files, duplicates):
-    '''Log start time, run time, files reviewed, and duplicates removed'''
+def log_usage(start_time, emls_scanned, duplicates):
+    '''Log start time, run time, emls scanned, and duplicates removed'''
     file_name = "usage_log.csv"
     run_time = datetime.now() - start_time
     if path.isfile(file_name):
         with open(file_name, "a", encoding='UTF-8') as file:
-            file.write(f"{start_time},{run_time},{files},{duplicates}\n")
+            file.write(f"{start_time},{run_time},{emls_scanned},{duplicates}\n")
     else:
         with open(file_name, "x", encoding='UTF-8') as file:
-            file.write("start time,run time,files scanned,duplicates\n")
-            file.write(f"{start_time},{run_time},{files},{duplicates}\n")
+            file.write("start time,run time,emls scanned,duplicates\n")
+            file.write(f"{start_time},{run_time},{emls_scanned},{duplicates}\n")
 
 def main():
     '''Remove duplicates, log usage info'''
     directory = input("Enter the full path to the folder which contains the EMLs: ").strip('"')
     start_time = datetime.now()
-    files, duplicates = remove_duplicates(directory)
+    emls_scanned, duplicates = remove_duplicates(directory)
     print(f"Removed {duplicates} duplicate .eml file{"s" if duplicates != 1 else ""}")
-    log_usage(start_time, files, duplicates)
+    log_usage(start_time, emls_scanned, duplicates)
 
 if __name__ == "__main__":
     main()
